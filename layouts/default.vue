41ec62daeeb46af0acab13b4087bfe8bd3e2c250
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-03-16 12:34:03 +0900

    Layoutの変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,39 @@
 <template>
-  <v-app>
-    <v-app-bar fixed app>
-      <v-toolbar-title v-text="title" />
+  <v-app dark>
+    <v-app-bar color="#F5B7B1 " class="white--text" app>
+      <v-toolbar-title to="/">
+        ストレスチェック
+      </v-toolbar-title>
       <v-spacer />
-      <v-btn icon>
-        <v-icon>mdi-menu</v-icon>
+      <v-btn
+        class="white--text"
+        icon
+        text
+        to="/"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-home-variant-outline</v-icon>
+      </v-btn>
+      <v-btn
+        class="white--text"
+        icon
+        text
+        to="/login"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+      <v-btn
+        class="white--text"
+        icon
+        text
+        to="/signup"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-account-multiple-plus</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -12,8 +41,11 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }} — <strong>ストレスチェック</strong></span>
     </v-footer>
   </v-app>
 </template>
