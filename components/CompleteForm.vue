2969f3b1fbbf965200710def1fb6d743e6f92160
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-03-18 15:29:56 +0900

    CompleteFormのデザイン

--- components/CompleteForm.vue
+++ components/CompleteForm.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-card color="transparent" height="500">
-    <v-row v-if="!isShow" class="fill-height" align="center" justify="center">
+    <v-row v-if="!isShow" class="mt-1" align="center" justify="center">
       <v-col cols="12" sm="12" md="12">
-        <v-card-text class="text-h6">ストレスチェックが終了しました。</v-card-text>
-        <v-card-text class="text-h6">ありがとうございました。</v-card-text>
-        <v-card-text>
+        <v-card-text class="text-h6" align="center" justify="center">ストレスチェックは以上です。</v-card-text>
+        <v-card-text class="text-h6" align="center" justify="center">ありがとうございました。</v-card-text>
+        <v-card-text align="center" justify="center">
           <v-btn color="primary" @click="showResult">
           結果表示
         </v-btn>
         </v-card-text>
       </v-col>
     </v-row>
-    <!-- <result-form v-else :total="total" :stress="stress" /> -->
     <result-form v-else />
-  </v-card>
 </template>
 
 <script>
